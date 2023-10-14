489aa0049a52e601593fedde35d50aef120574f3
Jordan Ahlers <jahlers46@gmail.com>
2023-10-14 14:31:18 -0700

    Added deckviewer component

---
+++ mtg-toolkit-frontend/src/Home.tsx
@@ -0,0 +1,7 @@
+
+function Home(){
+    const name = "Jordan"
+    return <h1>Hello {name}</h1>;
+}
+
+export default Home;
---
+++ mtg-toolkit-frontend/src/components/DeckViewer.tsx
@@ -0,0 +1,5 @@
+function DeckViewer(){
+    return <h1>Welcom To DeckViewer</h1>
+}
+
+export default DeckViewer;
